7efb6bb5870d0ac2fdc60d50ca8e0a57f050b6f8
Svart0x <110288743+Svart0x@users.noreply.github.com>
2022-08-03 15:29:30 +0200

    Added Stickers Pack Image and Title Preview

--- lsd.py
+++ lsd.py
@@ -4,28 +4,35 @@
 
 
 
+from turtle import title
 from PyQt6 import QtCore, QtGui, QtWidgets
+import PyQt6
+from PyQt6.QtGui import QPixmap, QScreen
+import sys,os
 import urllib.request, urllib.error
-import sys, os
+import requests
+from bs4 import BeautifulSoup
+import re
 
 basedir = os.path.dirname(__file__)
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(583, 239)
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("iconLineStickerApp.png"), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
-        MainWindow.setWindowIcon(icon)
+        MainWindow.setWindowModality(QtCore.Qt.WindowModality.ApplicationModal)
+        MainWindow.resize(612, 694)
+        font = QtGui.QFont()
+        font.setPointSize(1)
+        MainWindow.setFont(font)
+        #icon = QtGui.QIcon()
+        #icon.addPixmap(QtGui.QPixmap("icon.icns"), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
+        #MainWindow.setWindowIcon(icon)
         MainWindow.setAutoFillBackground(False)
-        MainWindow.setStyleSheet("QWidget {\n"
-"\n"
-"background : #565656\n"
-"\n"
-"}")
+        MainWindow.setStyleSheet("")
         MainWindow.setLocale(QtCore.QLocale(QtCore.QLocale.Language.English, QtCore.QLocale.Country.UnitedKingdom))
         self.pushButton = QtWidgets.QPushButton(MainWindow)
-        self.pushButton.setGeometry(QtCore.QRect(220, 150, 151, 51))
+        self.pushButton.setGeometry(QtCore.QRect(220, 620, 151, 51))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.pushButton.setFont(font)
@@ -35,14 +42,14 @@ class Ui_MainWindow(object):
         self.pushButton.setIconSize(QtCore.QSize(50, 50))
         self.pushButton.setObjectName("pushButton")
         self.layoutWidget = QtWidgets.QWidget(MainWindow)
-        self.layoutWidget.setGeometry(QtCore.QRect(80, 30, 403, 49))
+        self.layoutWidget.setGeometry(QtCore.QRect(60, 30, 469, 62))
         self.layoutWidget.setObjectName("layoutWidget")
         self.gridLayout = QtWidgets.QGridLayout(self.layoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setObjectName("gridLayout")
         self.explanationLabel = QtWidgets.QLabel(self.layoutWidget)
         font = QtGui.QFont()
-        font.setPointSize(12)
+        font.setPointSize(14)
         font.setBold(True)
         font.setItalic(False)
         font.setUnderline(False)
@@ -54,7 +61,7 @@ class Ui_MainWindow(object):
         self.gridLayout.addWidget(self.explanationLabel, 0, 0, 1, 2)
         self.label = QtWidgets.QLabel(self.layoutWidget)
         font = QtGui.QFont()
-        font.setPointSize(12)
+        font.setPointSize(14)
         font.setBold(True)
         font.setWeight(75)
         self.label.setFont(font)
@@ -64,7 +71,7 @@ class Ui_MainWindow(object):
         self.gridLayout.addWidget(self.label, 1, 0, 1, 1)
         self.stickerIDInput = QtWidgets.QLineEdit(self.layoutWidget)
         font = QtGui.QFont()
-        font.setPointSize(10)
+        font.setPointSize(16)
         self.stickerIDInput.setFont(font)
         self.stickerIDInput.setStyleSheet("QlineEdit\n"
 "{\n"
@@ -76,28 +83,100 @@ class Ui_MainWindow(object):
         self.stickerIDInput.setObjectName("stickerIDInput")
         self.gridLayout.addWidget(self.stickerIDInput, 1, 1, 1, 1)
 
-        # CREATION DE LA PROGRESSBAR
+
         self.progressBar = QtWidgets.QProgressBar(MainWindow)
-        self.progressBar.setGeometry(QtCore.QRect(80, 100, 441, 23))
+        self.progressBar.setGeometry(QtCore.QRect(80, 540, 441, 23))
         self.progressBar.setProperty("value", 0)
         self.progressBar.setObjectName("progressBar")
+        self.checkStickerButton = QtWidgets.QPushButton(MainWindow)
+        self.checkStickerButton.setGeometry(QtCore.QRect(220, 110, 164, 31))
+
+
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setKerning(True)
+
+
+        self.checkStickerButton.setFont(font)
+        self.checkStickerButton.setObjectName("checkStickerButton")
+        self.stickerPackDescription = QtWidgets.QLabel(MainWindow)
+        self.stickerPackDescription.setGeometry(QtCore.QRect(60, 160, 481, 53))
+
+
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(False)
+        font.setWeight(50)
+
+
+        self.stickerPackDescription.setFont(font)
+        self.stickerPackDescription.setFrameShape(QtWidgets.QFrame.Shape.NoFrame)
+        self.stickerPackDescription.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.stickerPackDescription.setIndent(-1)
+        self.stickerPackDescription.setObjectName("stickerPackDescription")
+
+
+        """self.stickerImage = QtWidgets.QGraphicsView(MainWindow)
+        self.stickerImage.setGeometry(QtCore.QRect(160, 250, 271, 211))
+        self.stickerImage.setFrameShadow(QtWidgets.QFrame.Shadow.Plain)
+        self.stickerImage.setObjectName("stickerImage")"""
+
+        self.stickerImage = QtWidgets.QLabel(MainWindow)
+        #self.stickerImage.setGeometry(160, 250, 271, 211)
+        self.stickerImage.setGeometry(160, 250, 240, 240)
+
+
 
         self.retranslateUi(MainWindow)
         self.pushButton.clicked.connect(self.Download) # type: ignore
+        self.checkStickerButton.clicked.connect(self.checkButtonClicked)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Line Sticker Downloader - Made Possible By Svart0x"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Line Sticker Downloader - Made By Svart0x"))
         self.pushButton.setText(_translate("MainWindow", "Download"))
         self.explanationLabel.setText(_translate("MainWindow", "Please enter in the box below the Line Sticker ID"))
-        self.label.setText(_translate("MainWindow", "Line Sticker ID :"))
+        self.label.setText(_translate("MainWindow", "Line Stickers Pack ID :"))
+        #self.stickerImage.setText(_translate("MainWindow", "IMAGE HERE"))
+        self.checkStickerButton.setText(_translate("MainWindow", "Check Sticker Pack"))
+        self.stickerPackDescription.setText(_translate("MainWindow", "TextLabel"))
+
+      
+        
+    def checkButtonClicked(self):
+        inputID = self.stickerIDInput.text()
+        page = requests.get(f"https://store.line.me/stickershop/product/{inputID}/en")
+        imageTag = f"https://stickershop.line-scdn.net/stickershop/v1/product/{inputID}/LINEStorePC/main.png"
+        parsedPAge = BeautifulSoup(page.content, 'lxml')
+        path3 = R"${TEMP}" + inputID + ".png"
+
+        #save_loc = os.path.expanduser("~/Downloads/" + inputID + ".png")
+        save_loc = os.path.expanduser(path3)
+        
+
+        
+        titleTag = parsedPAge.find('p', {'class':'mdCMN38Item01Ttl'})
+        title = titleTag.contents[0]
+        title2 = title.text
+        #print(title2)
+                          
+        self.stickerPackDescription.setText(title2)
+
+        reqImg = urllib.request.urlretrieve(imageTag,save_loc)
+
+        pixmap = QPixmap(os.path.expanduser(path3))
+        self.stickerImage.show()
+        self.stickerImage.setPixmap(pixmap)
+        
+
 
     def Handle_Progress(self, blocknum, blocksize, totalsize):
- 
-        ## calculate the progress
-        readed_data = int(blocknum) * int(blocksize)
-        self.progressBar.setValue(0)
+    
+        ## calculate the progress #####################
+        readed_data = int(blocknum) * int(blocksize) ##
+        self.progressBar.setValue(0)                 ##
+        ###############################################
 
         if totalsize > 0:
             download_percentage = readed_data * 100 / totalsize
@@ -106,19 +185,21 @@ class Ui_MainWindow(object):
 
     def Download(self):
         
+        
         inputID = self.stickerIDInput.text()
-        #hurl = f'http://dl.stickershop.line.naver.jp/products/0/0/1/{inputID}/iphone/stickerpack@2x.zip'
-        # specify the url of the file which is to be downloaded
-        down_url = f'http://dl.stickershop.line.naver.jp/products/0/0/1/{inputID}/iphone/stickerpack@2x.zip' # specify download url here
-        down_url2 = f'http://dl.stickershop.line.naver.jp/products/0/0/1/{inputID}/iphone/stickers@2x.zip' # specify download url here
+        page = requests.get(f"https://store.line.me/stickershop/product/{inputID}/en")
+        parsedPAge = BeautifulSoup(page.content, 'lxml')
+        titleTag = parsedPAge.find('p', {'class':'mdCMN38Item01Ttl'})
+        title = titleTag.contents[0]
+        title2 = title.text
+        #print(title2)
 
-        # specify save location where the file is to be saved
-        #save_loc = f'{inputID}.zip'
+        down_url = f'http://dl.stickershop.line.naver.jp/products/0/0/1/{inputID}/iphone/stickerpack@2x.zip' 
+        down_url2 = f'http://dl.stickershop.line.naver.jp/products/0/0/1/{inputID}/iphone/stickers@2x.zip' 
+
         save_loc = os.path.expanduser("~/Downloads/" + inputID + ".zip")
  
         
-
-        # Downloading using urllib
         try:
             req = urllib.request.urlretrieve(down_url,save_loc, self.Handle_Progress)
 
@@ -129,7 +210,7 @@ class Ui_MainWindow(object):
                 print('HTTPError: {}'.format(e.code))
         except urllib.error.URLError as e:
                 # Not an HTTP-specific error (e.g. connection refused)
-                # ...
+                
             print('URLError: {}'.format(e.reason))
         '''else:
             # 200
